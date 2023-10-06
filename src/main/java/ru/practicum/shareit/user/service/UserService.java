ed7be3fd1752a0b030b55ae2650f0549c977b3ed
Chernosmaga <sergeychernosmaga1997@yandex.ru>
2023-10-06 12:44:21 +0300

    feat: реализовал проект с учётом ТЗ 14 спринта

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -1,43 +1,17 @@
 package ru.practicum.shareit.user.service;
 
-import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.dto.UserDto;
 
 import java.util.List;
 
-/**
- * Класс-сервис для управления сущностями пользователей
- */
 public interface UserService {
-    /**
-     * Метод создаёт пользователя и добавляет его в хранилище
-     * @param user данные пользователя
-     * @return возвращает созданного пользователя
-     */
-    User create(User user);
-    /**
-     * Метод обновляет данные пользователя в хранилище
-     * @param id идентификатор пользователя
-     * @return возвращает новые данные о пользователе
-     */
+    UserDto create(UserDto user);
 
-    User update(Long id, User user);
-    /**
-     * Метод получения пользователя по идентификатору
-     * @param id идентификатор пользователя
-     * @return возвращает полученного пользователя
-     */
+    UserDto update(Long id, UserDto user);
 
-    User getById(Long id);
-    /**
-     * Метод удаления пользователя по идентификатору
-     * @param id идентификатор пользователя
-     */
+    UserDto getById(Long id);
 
     void deleteById(Long id);
-    /**
-     * Метод получения списка пользователей
-     * @return возвращает список пользователей из хранилища
-     */
 
-    List<User> getUsers();
+    List<UserDto> getUsers();
 }
